58825f037bde40ceaee7df131f69feb5d3d30bd6
MC-blone <lth666520@gmail.com>
2026-08-06 13:37:19 +0800

    Update module_fy4_lrit_data_decoder.h

--- plugins/fengyun4_support/fy4/module_fy4_lrit_data_decoder.h
+++ plugins/fengyun4_support/fy4/module_fy4_lrit_data_decoder.h
@@ -3,6 +3,7 @@
 #include "pipeline/modules/base/filestream_to_filestream.h"
 #include "xrit/processor/xrit_channel_processor.h"
 #include "xrit/xrit_file.h"
+#include <atomic>
 
 namespace fy4
 {
@@ -22,11 +23,18 @@ namespace fy4
             std::map<std::string, std::shared_ptr<satdump::xrit::XRITChannelProcessor>> all_processors;
             std::mutex all_processors_mtx;
 
-#if 0
-            std::map<int, uint64_t> decryption_keys;
-#endif
+            // DAT decryption related
+            bool is_dat_file;
+            char key_input[17];
+            bool key_entered;
+            std::vector<uint8_t> key_bytes;
+            std::atomic<bool> should_stop{false};
 
             void processLRITFile(satdump::xrit::XRITFile &file);
+            
+            // DES decryption functions
+            bool parse_key(const std::string& key_str, std::vector<uint8_t>& key_bytes);
+            std::vector<uint8_t> decrypt_dat_file(const std::vector<uint8_t>& data, const std::vector<uint8_t>& key);
 
         public:
             FY4LRITDataDecoderModule(std::string input_file, std::string output_file_hint, nlohmann::json parameters);
@@ -37,7 +45,7 @@ namespace fy4
         public:
             static std::string getID();
             virtual std::string getIDM() { return getID(); };
-            static nlohmann::json getParams() { return {}; } // TODOREWORK
+            static nlohmann::json getParams() { return {}; }
             static std::shared_ptr<ProcessingModule> getInstance(std::string input_file, std::string output_file_hint, nlohmann::json parameters);
         };
     } // namespace lrit
