d4b05afd8cd4a9b2d209b9cd33fe24bf10f22db9
MC-blone <lth666520@gmail.com>
2026-08-25 05:46:41 +0800

    更新 module_fy4_lrit_data_decoder.h

--- plugins/fengyun4_support/fy4/module_fy4_lrit_data_decoder.h
+++ plugins/fengyun4_support/fy4/module_fy4_lrit_data_decoder.h
@@ -3,50 +3,101 @@
 #include "pipeline/modules/base/filestream_to_filestream.h"
 #include "xrit/processor/xrit_channel_processor.h"
 #include "xrit/xrit_file.h"
+
+#include "fy4_des.h"
+
 #include <atomic>
+#include <cstdint>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
 
 namespace fy4
 {
     namespace lrit
     {
-        class FY4LRITDataDecoderModule : public satdump::pipeline::base::FileStreamToFileStreamModule
+        class FY4LRITDataDecoderModule :
+            public satdump::pipeline::base::FileStreamToFileStreamModule
         {
         protected:
             std::string directory;
+            std::string input_file_path;
 
             enum CustomFileParams
             {
                 IS_ENCRYPTED,
-                KEY_INDEX,
+                KEY_INDEX
             };
 
-            std::map<std::string, std::shared_ptr<satdump::xrit::XRITChannelProcessor>> all_processors;
+            std::map<
+                std::string,
+                std::shared_ptr<satdump::xrit::XRITChannelProcessor>
+            > all_processors;
+
             std::mutex all_processors_mtx;
 
-            // DAT decryption related
-            bool is_dat_file;
-            char key_input[17];
-            bool key_entered;
-            std::vector<uint8_t> key_bytes;
-            std::atomic<bool> should_stop{false};
+            std::map<
+                uint16_t,
+                std::vector<uint8_t>
+            > decryption_keys;
 
-            void processLRITFile(satdump::xrit::XRITFile &file);
-            
-            // DES decryption functions
-            bool parse_key(const std::string& key_str, std::vector<uint8_t>& key_bytes);
-            std::vector<uint8_t> decrypt_dat_file(const std::vector<uint8_t>& data, const std::vector<uint8_t>& key);
+            std::mutex decryption_keys_mtx;
+
+            std::atomic<uint64_t> bytes_received;
+            std::atomic<uint64_t> total_bytes;
+
+            bool keys_loaded;
+
+            bool loadEncryptionKeys();
+
+            bool findEncryptionKey(
+                uint16_t key_index,
+                std::vector<uint8_t> &key);
+
+            bool decryptLRITFile(
+                satdump::xrit::XRITFile &file,
+                uint16_t key_index);
+
+            void saveEncryptedFile(
+                satdump::xrit::XRITFile &file);
+
+            void saveLRITFile(
+                satdump::xrit::XRITFile &file);
+
+            void processLRITFile(
+                satdump::xrit::XRITFile &file);
 
         public:
-            FY4LRITDataDecoderModule(std::string input_file, std::string output_file_hint, nlohmann::json parameters);
+            FY4LRITDataDecoderModule(
+                std::string input_file,
+                std::string output_file_hint,
+                nlohmann::json parameters);
+
             ~FY4LRITDataDecoderModule();
+
             void process();
+
             void drawUI(bool window);
 
         public:
             static std::string getID();
-            virtual std::string getIDM() { return getID(); };
-            static nlohmann::json getParams() { return {}; }
-            static std::shared_ptr<ProcessingModule> getInstance(std::string input_file, std::string output_file_hint, nlohmann::json parameters);
+
+            virtual std::string getIDM()
+            {
+                return getID();
+            }
+
+            static nlohmann::json getParams()
+            {
+                return {};
+            }
+
+            static std::shared_ptr<ProcessingModule> getInstance(
+                std::string input_file,
+                std::string output_file_hint,
+                nlohmann::json parameters);
         };
-    } // namespace lrit
-} // namespace fy4
+    }
+}
